500eb24e31ad60528928ee482f5db60b71203cb0
Mohd Ayan Ansari <mohdayanansari1999@gmail.com>
2023-10-03 00:58:39 +0530

    fix: changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,19 @@ const page = (props: Props) => {
   return (
     <main>
       <header className="text-center h-[60px] flex items-center justify-center">
-        Home Page
+        Home Page/Landing Page
       </header>
       <div className="h-[calc(100vh-60px)] flex items-center justify-center">
         <Link
           href={"/dashboard"}
-          className="bg-app-accent-2 text-white/90 font-black text-6xl animate-bounce underline underline-offset-8 p-10 rounded-3xl hover:bg-app-tertiary animationEaseInOut"
+          className="bg-app-accent-2 text-white/90 font-black  text-2xl lg:text-6xl animate-bounce underline underline-offset-8 p-10 rounded-3xl hover:bg-app-tertiary animationEaseInOut"
         >
           Checkout the User Dashboard
         </Link>
         <footer className="fixed bottom-0 h-[60px]">
-          Created by Ayan Ansari
+          <Link href={"https://github.com/mohdayanansari"}>
+            Created by Mohd Ayan Ansari
+          </Link>
         </footer>
       </div>
     </main>
